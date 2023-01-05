7ea1fd36de642ccf16c8dfc7654c8d2d5f2516b1
Narin68 <pach.narin2018@rupp.edu.kh>
2023-01-05 18:54:00 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState, useEffect, createContext } from "react";
 import Axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Menu } from "./Pages/Menu";
 import { Contact } from "./Pages/Contact";
@@ -10,15 +10,10 @@ import { AppContext } from "./Pages/AppContext";
 import { Profile } from "./Pages/Profile";
 
 function App() {
-  const complete = (id) => {};
+  let [username, setUsername] = useState("");
 
-  // const fetch = (name) => {
-  //   Axios.get("https://api.agify.io/?name=" + name).then((res) => {
-  //     setAge(res.data.age);
-  //   })
-  // }
   return (
-    <div>
+    <div className="App">
       <AppContext.Provider value={{ username, setUsername }}>
         <Router>
           <Nav />
