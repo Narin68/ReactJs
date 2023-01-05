1b9f046f1f8ca0c021e1faff942913c78a73de91
Narin68 <pach.narin2018@rupp.edu.kh>
2023-01-05 18:33:18 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState, useEffect, createContext } from "react";
+import Axios from "axios";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Menu } from "./Pages/Menu";
+import { Contact } from "./Pages/Contact";
+import { Nav } from "./Pages/Nav";
+import { AppContext } from "./Pages/AppContext";
+import { Profile } from "./Pages/Profile";
+
 function App() {
+  const complete = (id) => {};
 
-  let [todoList, setTodoList] = useState([]);
-  let [task, setTask] = useState("");
-
-  const deleteTask = (id) => {
-    let newTask = todoList.filter((v) => v.id != id);
-    setTodoList(newTask);
-  }
-
-  const addTask = () => {
-    let task = { name: newTask, id: todoList.length == 0 ? 1 : todoList[todoList.length - 1].id + 1 };
-    
-    setTodoList([...todoList,task]);
-  }
-
-  const complete = (id) => {
-
-  }
-
-
-
-
+  // const fetch = (name) => {
+  //   Axios.get("https://api.agify.io/?name=" + name).then((res) => {
+  //     setAge(res.data.age);
+  //   })
+  // }
   return (
-    <div className="App">
-      
+    <div>
+      <AppContext.Provider value={{ username, setUsername }}>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/menu" element={<Menu />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
